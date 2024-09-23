056369089fd085c357eae7f92cbc6582e7713fae
Cristian Gutierrez <cdgutierrez456@gmail.com>
2024-09-23 15:25:25 -0500

    Init commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './components/Header'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <h1>This is my proyect</h1>
+    </>
+  )
+}
+
+export default App
